f49c1e97a23f448d91c768b7f57ddf727bd958af
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2024-04-08 15:38:08 +0300

    03_PnR_setup.tcl

--- IC Compiler II/Scripts/03_PnR_setup.tcl
+++ IC Compiler II/Scripts/03_PnR_setup.tcl
@@ -301,7 +301,8 @@ set_app_options -name time.disable_case_analysis -value false
 set_app_options -name place.coarse.continue_on_missing_scandef -value true
 set_app_options -name opt.common.user_instance_name_prefix -value place
 
-set_voltage 0.98
+set_app_options -list {time.delay_calculation_style auto}
+set_voltage 0.95
 place_opt
 legalize_placement
 check_legality -verbos
