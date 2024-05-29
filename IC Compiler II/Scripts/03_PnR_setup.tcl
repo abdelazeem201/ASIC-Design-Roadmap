ed70be88a4da396339d0b54cad0028fcf0d61cd9
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2024-05-29 11:34:47 +0300

    03_PnR_setup.tcl

--- IC Compiler II/Scripts/03_PnR_setup.tcl
+++ IC Compiler II/Scripts/03_PnR_setup.tcl
@@ -287,6 +287,33 @@ set_pg_strategy rail_strat -core \
     -pattern {{name: std_cell_rail} {nets: VDD VSS} }
     
 compile_pg -strategies rail_strat    
+################################################################################
+#-------------------------------------------------------------------------------
+# IR Drop Map
+#-------------------------------------------------------------------------------
+################################################################################
+set die_llx [lindex [lindex [ get_attribute [get_core_area] bbox] 0] 0]
+set die_lly [lindex [lindex [ get_attribute [get_core_area] bbox] 0] 1]
+set die_urx [lindex [lindex [ get_attribute [get_core_area] bbox] 1] 0]
+set die_ury [lindex [lindex [ get_attribute [get_core_area] bbox] 1] 1]
+
+for {set i "[expr $die_llx + 20]"} {$i < "[expr $die_urx - 40]"} {set i [expr $i + 80]} {
+	set_virtual_pad -net VSS -coordinate [format {%.1f %.1f} $i $die_lly]
+	set_virtual_pad -net VDD -coordinate [format {%.1f %.1f} [expr $i + 40] $die_lly]
+
+	set_virtual_pad -net VSS -coordinate [format {%.1f %.1f}  $i $die_ury]
+	set_virtual_pad -net VDD -coordinate [format {%.1f %.1f} [expr $i + 40] $die_ury]
+}
+for {set i "[expr $die_lly + 20]"} {$i < "[expr $die_ury - 40]"} {set i [expr $i + 80]} {
+	set_virtual_pad -net VSS -coordinate [format {%.1f %.1f} $die_llx $i]
+	set_virtual_pad -net VDD -coordinate [format {%.1f %.1f} $die_llx [expr $i + 40]]
+
+	set_virtual_pad -net VSS -coordinate [format {%.1f %.1f} $die_urx $i]
+	set_virtual_pad -net VDD -coordinate [format {%.1f %.1f} $die_urx [expr $i + 40] ]
+}
+
+analyze_power_plan -power_budget 250 -voltage 1.2 -nets {VDD VSS} -use_terminals_as_pads
+
 
 save_block
 ##############################################
