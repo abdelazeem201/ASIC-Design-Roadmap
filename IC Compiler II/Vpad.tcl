c50cdfa4f861eb03d724d67583dfaf1bd2a8592e
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2024-05-29 11:32:30 +0300

    Vpad.tcl

--- IC Compiler II/Vpad.tcl
+++ IC Compiler II/Vpad.tcl
@@ -1,7 +1,7 @@
-set die_llx [lindex [lindex [ get_attribute [get_die_area] bbox] 0] 0]
-set die_lly [lindex [lindex [ get_attribute [get_die_area] bbox] 0] 1]
-set die_urx [lindex [lindex [ get_attribute [get_die_area] bbox] 1] 0]
-set die_ury [lindex [lindex [ get_attribute [get_die_area] bbox] 1] 1]
+set die_llx [lindex [lindex [ get_attribute [get_core_area] bbox] 0] 0]
+set die_lly [lindex [lindex [ get_attribute [get_core_area] bbox] 0] 1]
+set die_urx [lindex [lindex [ get_attribute [get_core_area] bbox] 1] 0]
+set die_ury [lindex [lindex [ get_attribute [get_core_area] bbox] 1] 1]
 
 for {set i "[expr $die_llx + 20]"} {$i < "[expr $die_urx - 40]"} {set i [expr $i + 80]} {
 	set_virtual_pad -net VSS -coordinate [format {%.1f %.1f} $i $die_lly]
